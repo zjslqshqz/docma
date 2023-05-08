89b514d914080f75de08b46980e9b7d20e790971
chendongdong <564964001@qq.com>
2023-05-08 09:15:33 +0800

    remove templates/zebra

--- test/build.spec.js
+++ test/build.spec.js
@@ -14,50 +14,50 @@ describe('build', () => {
 
     // beforeAll(function () {});
 
-    // test('build with query-routing', async () => {
-    //     const config = {
-    //         debug: 30,
-    //         src: [
-    //             'test/input/*.js',
-    //             {
-    //                 'group': 'test/input/src/**.js',
-    //                 'html/route': 'test/input/html-test.html'
-    //             },
-    //             'test/input/md-test.md'
-    //         ],
-    //         dest: 'test/output/query-routing',
-    //         markdown: {
-    //             gfm: true,
-    //             tables: true,
-    //             breaks: false,
-    //             pedantic: false,
-    //             sanitize: false,
-    //             smartLists: true,
-    //             smartypants: false,
-    //             xhtml: false,
-    //             tasks: true,
-    //             emoji: true
-    //         },
-    //         app: {
-    //             title: 'Docs',
-    //             entrance: 'api:group',
-    //             routing: 'query',
-    //             base: '/'
-    //         },
-    //         template: {
-    //             path: './templates/zebra',
-    //             options: {
-    //                 title: ''
-    //             }
-    //         }
-    //     };
-    //     try {
-    //         await expect(Docma.create().build(config)).resolves.toEqual(true);
-    //     } catch (err) {
-    //         console.log(err.stack || err);
-    //         expect(Boolean(err)).toEqual(false);
-    //     }
-    // });
+    test('build with query-routing', async () => {
+        const config = {
+            debug: 30,
+            src: [
+                'test/input/*.js',
+                {
+                    'group': 'test/input/src/**.js',
+                    'html/route': 'test/input/html-test.html'
+                },
+                'test/input/md-test.md'
+            ],
+            dest: 'test/output/query-routing',
+            markdown: {
+                gfm: true,
+                tables: true,
+                breaks: false,
+                pedantic: false,
+                sanitize: false,
+                smartLists: true,
+                smartypants: false,
+                xhtml: false,
+                tasks: true,
+                emoji: true
+            },
+            app: {
+                title: 'Docs',
+                entrance: 'api:group',
+                routing: 'query',
+                base: '/'
+            },
+            template: {
+                path: './node_modules/zjslqshqz-docma-template-zebra',
+                options: {
+                    title: ''
+                }
+            }
+        };
+        try {
+            await expect(Docma.create().build(config)).resolves.toEqual(true);
+        } catch (err) {
+            console.log(err.stack || err);
+            expect(Boolean(err)).toEqual(false);
+        }
+    });
 
     // test.only('build with path-routing (for GitHub)', () => {
     //     config.app.routing = 'path';
@@ -88,7 +88,7 @@ describe('build', () => {
     //         });
     // });
 
-    test('build test',()=>{
-        return undefined;
-    });
+    // test('build test',()=>{
+    //     return undefined;
+    // });
 });
